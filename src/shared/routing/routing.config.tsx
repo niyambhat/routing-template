9dbe86f21faa652eab3bb0650df2c58f59cab54c
neom <69999426+niyambhat@users.noreply.github.com>
2024-07-22 01:29:54 +0000

    routes config

---
+++ src/shared/routing/routing.config.tsx
@@ -0,0 +1,25 @@
+// <------------------>
+//  Page Names
+// <------------------>
+
+export enum Pages {
+    About = "About Us",
+    Contact = "Contact",
+    Home = "Home",
+    Products = "Our Products",
+    Services = "Our Services",
+    Blog = "Blog",
+    FAQ = "FAQ",
+    PrivacyPolicy = "Privacy Policy",
+    TermsOfService = "Terms of Service"
+}
+
+export enum ErrorPages {
+    SessionTimeOut="Session timed out",
+    SystemUnavialable = "System unavailable"
+}
+
+export enum ErrorPageHeading {
+    SessionTimeOut="Oh no!",
+    SystemUnavialable = "Oh no!"
+}
